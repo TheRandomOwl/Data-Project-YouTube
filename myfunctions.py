b1b556d03ff44d46f227b3f457ec789880c75594
John Doe <46622921+TheRandomOwl@users.noreply.github.com>
2024-05-06 12:42:14 -0700

    add new function

--- myfunctions.py
+++ myfunctions.py
@@ -42,6 +42,42 @@ def filter_or(data, filters):
                 break
     return new_list
 
+def entry_max(data, key):
+    """
+    Find the entry with the maximum value for the given key in a list of dictionaries.
+
+    Args:
+        data (list): A list of dictionaries.
+        key (str): The key to compare the values.
+
+    Returns:
+        dict: The dictionary entry with the maximum value for the given key.
+              If the list is empty or the key does not exist in any of the dictionaries, an empty dictionary is returned.
+
+    Examples:
+        >>> data = [{'key1': 1, 'key2': 2}, {'key1': 3, 'key2': 4}, {'key1': 5, 'key2': 6}, {'key1': 'invalid entry', 'key2': 4}]
+        >>> entry_max(data, 'key1')
+        {'key1': 5, 'key2': 6}
+        >>> entry_max(data, 'nonexistent key')
+        {}
+        >>> entry_max([], 'key2')
+        {}
+    """
+    if data == []:
+        return {}
+    max_entry = data[0]
+    for entry in data:
+        try:
+            if entry[key] > max_entry[key]:
+                max_entry = entry
+        except TypeError:
+            # skip invalid entries
+            pass
+        except KeyError:
+            # return empty dictionary if key does not exist
+            return {}
+    return max_entry
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()

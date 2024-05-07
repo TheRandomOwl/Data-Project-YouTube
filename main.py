2f9e67077c01ee84f3091b264bf82a5cf7519272
John Doe <46622921+TheRandomOwl@users.noreply.github.com>
2024-05-06 21:43:25 -0700

    correct incorrect data

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ from myfunctions import *
 import csv
 import pprint
 
+def correct_data(data):
+    """ 
+    https://en.wikipedia.org/wiki/1996_Charkhi_Dadri_mid-air_collision
+    """
+    for row in data:
+        if row["Accident.Number"] == "DCA97WA007B":
+            row["Model"] = "Il-76TD"
+            row["Make"] = "Ilyushin"
+            row["Total.Fatal.Injuries"] = "37"
+        if row["Accident.Number"] == "DCA97WA007A":
+            row["Total.Fatal.Injuries"] = "312"
+
 def main():
     # get directory for csv file AviationData.csv
     csv_dir = 'data/AviationData.csv'
@@ -16,25 +28,15 @@ def main():
         reader = csv.DictReader(file)
         # create a list of dictionaries
         data = list(reader)
+    correct_data(data)
 
     # print a numbered list of the keys in the dictionary
     for i, key in enumerate(data[0].keys()):
         print(f"{i+1}. {key.replace('.', ' ')}")
 
-    # get user input for key
-    index = int(input("Enter the number of the key you want to filter by: ")) - 1
-    
-    # Get the selected key
-    selected_key = list(data[0].keys())[index]
-
-    # Get the unique values for the selected key
-    unique_values = set(row[selected_key] for row in data)
-
-    # Print the numbered list of unique values
-    for i, value in enumerate(unique_values):
-        print(f"{i+1}. {value}")
-
-    pprint.pprint(filter_and(data, {'Model':'DC9', 'Country': 'United States'}))
+    # pprint.pprint(filter_and(data, {'Model':'DC9', 'Country': 'United States'}))
+    # pprint.pprint(filter_or(data, {'Event.Date': '2001-09-11'}))
+    pprint.pprint(entry_max(data, 'Total.Fatal.Injuries'))
 
 if __name__ == '__main__':
     main()

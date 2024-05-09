660bb6cdb71c868356a067312a8b0fe2654dc615
John Doe <46622921+TheRandomOwl@users.noreply.github.com>
2024-05-08 17:03:58 -0700

    update main.py for new data

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
 """ 
-CSV file: AviationData.csv
-Kaggle: https://www.kaggle.com/datasets/yassereleraky/aviation-accident-ntsb
+CSV file: Global-YouTube-Statistics.csv.csv
+Kaggle: https://www.kaggle.com/datasets/nelgiriyewithana/global-youtube-statistics-2023
 """
 from myfunctions import *
 import csv
 import pprint
 
-def correct_data(data):
-    """ 
-    https://en.wikipedia.org/wiki/1996_Charkhi_Dadri_mid-air_collision
-    """
-    for row in data:
-        if row["Accident.Number"] == "DCA97WA007B":
-            row["Model"] = "Il-76TD"
-            row["Make"] = "Ilyushin"
-            row["Total.Fatal.Injuries"] = "37"
-        if row["Accident.Number"] == "DCA97WA007A":
-            row["Total.Fatal.Injuries"] = "312"
-
 def main():
     # get directory for csv file AviationData.csv
-    csv_dir = 'data/AviationData.csv'
+    csv_dir = 'data/Global-YouTube-Statistics.csv'
     
     # open csv file
     with open(csv_dir, 'r', encoding='latin-1') as file:
@@ -28,15 +16,10 @@ def main():
         reader = csv.DictReader(file)
         # create a list of dictionaries
         data = list(reader)
-    correct_data(data)
 
     # print a numbered list of the keys in the dictionary
     for i, key in enumerate(data[0].keys()):
         print(f"{i+1}. {key.replace('.', ' ')}")
-
-    # pprint.pprint(filter_and(data, {'Model':'DC9', 'Country': 'United States'}))
-    # pprint.pprint(filter_or(data, {'Event.Date': '2001-09-11'}))
-    pprint.pprint(entry_max(data, 'Total.Fatal.Injuries'))
 
 if __name__ == '__main__':
     main()
